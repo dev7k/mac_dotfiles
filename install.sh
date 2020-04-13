9cce4f1f597daa741ffac6e908c4fb911aefd374
dev7k <dev7k@protonmail.com>
2020-04-13 20:36:24 +0200

    Added main install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+# Install Brew Packages
+brew install git
+brew install tig
+brew install htop
+brew install midnight-commander
+brew install sl
+brew install speedtest-cli
+brew install tmux
+brew install tree
+
+# Install zsh with oh-my-zsh
+brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install fzf
+brew install fzf
+/usr/local/opt/fzf/install
+
+# Clone zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
