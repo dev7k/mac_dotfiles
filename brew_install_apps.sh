7af75bf5efd2a9bb50c6286dd788e7d02f2eb3fe
dev7k <dev7k@protonmail.com>
2020-04-13 20:38:46 +0200

    Added application install script

---
+++ brew_install_apps.sh
@@ -0,0 +1,37 @@
+# Install MacOS application (development)
+brew cask install docker
+brew cask install dotnet-sdk
+brew cask install fork
+brew cask install iterm2
+brew cask install sublime-text
+brew cask install visual-studio-code
+
+# Install MacOS applications
+brew cask install appcleaner
+brew cask install atext
+brew cask install caffeine
+brew cask install clipy
+brew cask install coconutbattery
+brew cask install commander-one
+brew cask install console
+brew cask install firefox
+brew cask install forklift
+brew cask install gemini
+brew cask install google-chrome
+brew cask install iina
+brew cask install karabiner-elements
+brew cask install keka
+brew cask install keybase
+brew cask install nordvpn
+brew cask install pdf-expert
+brew cask install qnapi
+brew cask install skim
+brew cask install skype
+brew cask install spectacle
+
+# Install quickview plugins
+brew cask install qlcolorcode
+brew cask install qlimagesize
+brew cask install qlmarkdown
+brew cask install qlstephen
+brew cask install quicklook-json
