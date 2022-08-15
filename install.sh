e01c712f61a518b8bee721a496d7d12f8c95e24e
dev7k <dev7k@protonmail.com>
2022-08-15 18:17:14 +0200

    Updated installation script

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 # Install Brew Packages
 brew install htop
+brew install wget
 brew install midnight-commander
 brew install tmux
 brew install svn
-brew tap homebrew/cask-fonts && brew install homebrew/cask-fonts/font-source-code-pro
+brew install exa
+brew install bat
+brew install lsd
+
+# Install fonts
+brew tap homebrew/cask-fonts
+brew install font-source-code-pro
+brew install font-jetbrains-mono-nerd-font
 
 # Install MacOS application (development)
 brew install homebrew/cask/docker
@@ -13,7 +21,6 @@ brew install iterm2
 brew install sublime-text
 brew install visual-studio-code
 brew install visual-studio-code-insiders
-brew install --cask lepton
 
 # Install MacOS applicatiion
 brew install macpass
